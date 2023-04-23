ed0ad99f9c3416b31026585efa2e0a60c51bf007
Nathan Wilce <me@nwilce.co.uk>
2023-04-23 21:17:12 +0000

    initial

---
+++ app.py
@@ -0,0 +1,49 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+from selenium_pool import SeleniumPool
+import atexit
+import asyncio
+from aiohttp import web
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+pool = SeleniumPool(5)
+
+executor = ThreadPoolExecutor()
+
+async def index(request):
+    args = request.query
+    
+    if 'url' not in args:
+        return web.Response(text='No url provided')
+    
+    instance = pool.get_instance()
+    instance.get(args['url'])
+    
+    # wait for page to load
+    while instance.execute_script('return document.readyState') != 'complete':
+        await asyncio.sleep(0.01)
+        
+    # wait for loading icon to disappear
+    wait = WebDriverWait(instance, 10)
+    
+    try:
+        loading_icon = await asyncio.get_event_loop().run_in_executor(executor, wait.until, EC.invisibility_of_element_located((By.CSS_SELECTOR, '[class*="loader"], [id*="loader"]')))
+    except:
+        return web.Response(text='Timed out (10 seconds) waiting for loading icon to disappear')
+        
+    pool.release_instance(instance)
+        
+    return web.Response(content_type='text/html', text=instance.page_source)
+
+def cleanup():
+    pool.destroy_pool()
+
+if __name__ == '__main__':
+    # register event to kill all selenium instances on exit
+    atexit.register(cleanup)
+
+    app = web.Application()
+    app.add_routes([web.get('/', index)])
+    web.run_app(app, port=8080)
---
+++ selenium_pool.py
@@ -0,0 +1,76 @@
+import time
+from selenium import webdriver
+import threading
+
+
+class SeleniumPool:
+    def __init__(self, pool_size):
+        self.pool_size = pool_size
+        self.pool = []
+        for i in range(pool_size):
+            self.pool.append(self.create_instance())
+            
+        # start worker thread to reset pool every 5 minutes
+        threading.Thread(target=self.worker_thread).start()
+        
+    def create_instance(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized") # open Browser in maximized mode
+        options.add_argument("disable-infobars") # disabling infobars
+        options.add_argument("--disable-extensions") # disabling extensions
+        options.add_argument("--disable-gpu") # applicable to windows os only
+        options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
+        options.add_argument("--no-sandbox") # Bypass OS security model
+        # headless
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(options=options)
+        driver.in_use = False # add in_use flag
+        return driver
+    
+    def get_instance(self):
+        while True:
+            for instance in self.pool:
+                if not instance.in_use:
+                    instance.in_use = True
+                    return instance
+            time.sleep(1)
+    
+    def release_instance(self, instance):
+        instance.in_use = False
+        
+    def destroy_pool(self):
+        for instance in self.pool:
+            while instance.in_use:
+                time.sleep(1)
+            instance.quit()
+        self.pool = []
+        
+    def repair_pool(self):
+        for instance in self.pool:
+            try:
+                instance.title # check if instance is still usable
+            except:
+                self.pool.remove(instance)
+                instance.quit()
+                new_instance = self.create_instance()
+                self.pool.append(new_instance)
+
+    def reset_pool(self):
+        self.destroy_pool()
+        for i in range(self.pool_size):
+            self.pool.append(self.create_instance())
+            
+    def worker_thread(self):
+        pool_reset = time.time()
+        pool_repair = time.time()
+        while True:
+            if time.time() - pool_reset > 300:
+                self.reset_pool()
+                pool_reset = time.time()
+                
+            if time.time() - pool_repair > 60:
+                self.repair_pool()
+                pool_repair = time.time()
+                
+            time.sleep(1)
+            
